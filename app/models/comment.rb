e504091a3ae0a7868c4005c119ae9f5c101065a6
Chris Blackburn <cblackburn@cbciweb.com>
2010-01-07 17:07:32 -0600

    added some validations and dependent destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  validates_presence_of :username, :body
+  validates_format_of :username, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})/i
 end
